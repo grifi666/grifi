c173b53afacdc37fa64198849774d8d2772a4560
grifi666 <foxmine9@gmail.com>
2025-04-19 05:10:15 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+function switchTab(tabElement, tabId) {
+  document.querySelectorAll('.tab').forEach(el => el.classList.remove('active'));
+  document.querySelectorAll('.tab-content').forEach(el => el.classList.remove('active'));
 
+  tabElement.classList.add('active');
+  document.getElementById(tabId).classList.add('active');
+}
+
+function previewImage(event) {
+  const reader = new FileReader();
+  reader.onload = function () {
+    const img = document.getElementById('id-photo');
+    img.src = reader.result;
+    img.style.display = 'block';
+  };
+  reader.readAsDataURL(event.target.files[0]);
+}
